bf87d1c492aaf60ea2bb1de6e520485963c4ae97
PeterZajac <zajacp011@gmail.com>
2025-02-01 17:37:10 +0100

    installing mantine
    refactor(App): remove unnecessary code and simplify component structure
    
    feat(Navbar): addin navbar
    
    feat(Footer): add footer component with styling and links
    
    feat(Hero): add Hero component with styling and content; updating LOGOs
    
    refactor: updating design of navbar, footer, hero section.. Section wrapper component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "@mantine/core/styles.css";
+
 import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import { Footer, Navbar } from "./components/global";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -9,6 +12,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <Navbar />
+      <App />
+      <Footer />
+    </MantineProvider>
   </React.StrictMode>,
 );
