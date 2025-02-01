dcd2b86141d921c01157fcbac2473bba7d3c9204
PeterZajac <zajacp011@gmail.com>
2025-02-01 17:37:10 +0100

    installing mantine

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@mantine/core/styles.css";
+
 import "./index.css";
 import App from "./App";
+import { MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <MantineProvider> 
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
 );
