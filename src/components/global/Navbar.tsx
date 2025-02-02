6b6c608576169f26702c9f96a273ead64b2f974a
PeterZajac <zajacp011@gmail.com>
2025-02-02 16:44:45 +0100

    feat(Navbar): addin navbar

---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,85 @@
+import {
+  Box,
+  Burger,
+  Button,
+  Divider,
+  Drawer,
+  Group,
+  ScrollArea,
+  useMantineTheme,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import classes from "./Navbar.module.css";
+
+const Navbar = () => {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const theme = useMantineTheme();
+
+  const links = [
+    { title: "Home", href: "#" },
+    { title: "Courses", href: "#" },
+    { title: "Teachers", href: "#" },
+    { title: "Student Gallery", href: "#" },
+    { title: "Contact", href: "#" },
+  ];
+  return (
+    <Box pb={0}>
+      <header className={classes.header}>
+        <Group justify="space-between" h="100%">
+          {/* LOGO SWAP */}
+          <MantineLogo size={30} />
+
+          <Group h="100%" gap={15} visibleFrom="sm" align="right">
+            {links.map((link) => (
+              <a href={link.href} className={classes.link}>
+                {link.title}
+              </a>
+            ))}{" "}
+          </Group>
+
+          <Group visibleFrom={"sm"}>
+            <Button color={theme.colors.orange[6]}>Log in</Button>
+            <Button color="black">Sign up</Button>
+          </Group>
+
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            hiddenFrom="sm"
+          />
+        </Group>
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="80%"
+        padding="md"
+        title="--logo-"
+        hiddenFrom="sm"
+        zIndex={1000000}
+        position="right"
+        transitionProps={{ transition: "slide-left", duration: 550 }}
+      >
+        <ScrollArea h="calc(100vh - 80px" mx="-md">
+          <Divider my="sm" />
+          {links.map((link) => (
+            <a href={link.href} className={classes.link}>
+              {link.title}
+            </a>
+          ))}
+          <Divider my="sm" />
+
+          <Group justify="center" grow pb="xl" px="md">
+            <Button color={theme.colors.orange[6]}>Log in</Button>
+            <Button color="black">Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Navbar;
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "@mantine/core/styles.css";
 
-import "./index.css";
 import App from "./App";
 import { MantineProvider } from "@mantine/core";
+import Navbar from "./components/global/Navbar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -13,6 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <MantineProvider>
+      <Navbar />
       <App />
     </MantineProvider>
   </React.StrictMode>,
