4100653eb1e4b07a530732eb34d979670e9e48fd
PeterZajac <zajacp011@gmail.com>
2025-02-07 17:58:10 +0100

    feat(FAQItem): enhance styling for question and answer text in FAQItem component

--- src/components/Landing/FAQSection/FAQItem.tsx
+++ src/components/Landing/FAQSection/FAQItem.tsx
@@ -5,8 +5,10 @@ import { FAQItemProps } from "@/types";
 const FAQItems = ({ question, answer, value, icon }: FAQItemProps) => {
   return (
     <Accordion.Item className={classes.item} value={value}>
-      <Accordion.Control icon={icon}>{question}</Accordion.Control>
-      <Accordion.Panel>{answer}</Accordion.Panel>
+      <Accordion.Control icon={icon} fz={19} fw={500}>
+        {question}
+      </Accordion.Control>
+      <Accordion.Panel fz={14}>{answer}</Accordion.Panel>
     </Accordion.Item>
   );
 };
